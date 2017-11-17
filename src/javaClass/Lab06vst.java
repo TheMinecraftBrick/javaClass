52f71b6af577397a1dba809ce74b21cdb941ebdd
Lego Dude <kushdalal007@gmail.com>
2017-11-17 08:56:50 -0500

    Fixed thing with lab. not much else

--- src/javaClass/Lab06vst.java
+++ src/javaClass/Lab06vst.java
@@ -12,15 +12,16 @@ import java.applet.*;
 import java.util.*;
 
 public class Lab06vst extends Applet {
-	/* ignore all this for now
-	 
-	  public static void color(Color colour){
-		int r = (int) (Math.random() *(255)); //Choose a random color between 0 and 255 (RGB colors 0-255)
-		int gr = (int) (Math.random() *(255));
-		int b = (int) (Math.random() *(255));
-		
-		colour = new Color(r, gr, b); // define colour as red green and blue numbers
-	}*/
+	/*
+	 * ignore all this for now
+	 * 
+	 * public static void color(Color colour){ int r = (int) (Math.random() *(255));
+	 * //Choose a random color between 0 and 255 (RGB colors 0-255) int gr = (int)
+	 * (Math.random() *(255)); int b = (int) (Math.random() *(255));
+	 * 
+	 * colour = new Color(r, gr, b); // define colour as red green and blue numbers
+	 * }
+	 */
 
 	public void paint(Graphics g) {
 		// Draw Grid
@@ -29,96 +30,97 @@ public class Lab06vst extends Applet {
 		g.drawLine(10, 300, 790, 300);
 
 		// Draw Random Lines
-		//Our beautiful "for" loop to get those 100 lines so we don't have to copy paste this 100 times
+		// Our beautiful "for" loop to get those 100 lines so we don't have to copy
+		// paste this 100 times
 		for (int k = 1; k <= 100; k++) {
-			
-			//Random Color generator
-			int r = (int) (Math.random() *(256)); //Choose a random color between 0 and 255 (RGB colors 0-255)
-			int gr = (int) (Math.random() *(256));
-			int b = (int) (Math.random() *(256));
+
+			// Random Color generator
+			int r = (int) (Math.random() * (256)); // Choose a random color between 0 and 255 (RGB colors 0-255)
+			int gr = (int) (Math.random() * (256));
+			int b = (int) (Math.random() * (256));
 			Color colour = new Color(r, gr, b); // define colour as red green and blue numbers
 			g.setColor(colour); // set the color of them lines to that random color we made
-			
-			//The random line generator
-			int x = 10 + (int) (Math.random() *(400 - 10) + 1); // integer x is a random number between 10 and 400
-			int y = 10 + (int) (Math.random() *(300 - 10) + 1); // integer x is a random number between 10 and 300
-			int x1 = 10 + (int) (Math.random() *(400 - 10) + 1);
-			int y1 = 10 + (int) (Math.random() *(300 - 10) + 1);
-			
-			System.out.println(x + " - " + k); //Proof / Checking for if its 100 lines
-			
-			g.drawLine(x, y, x1, y1); //draw the lines here
+
+			// The random line generator
+			int x = 10 + (int) (Math.random() * (400 - 10) + 1); // integer x is a random number between 10 and 400
+			int y = 10 + (int) (Math.random() * (300 - 10) + 1); // integer x is a random number between 10 and 300
+			int x1 = 10 + (int) (Math.random() * (400 - 10) + 1);
+			int y1 = 10 + (int) (Math.random() * (300 - 10) + 1);
+
+			System.out.println(x + " - " + k); // Proof / Checking for if its 100 lines
+
+			g.drawLine(x, y, x1, y1); // draw the lines here
 		}
 
 		// Draw Random s4s
-		//for loop again.. you get the idea
+		// for loop again.. you get the idea
 		for (int k = 1; k <= 100; k++) {
-			
-			//color module
-			int r = (int) (Math.random() *(256)); //Choose a random color between 0 and 255 (RGB colors 0-255)
-			int gr = (int) (Math.random() *(256));
-			int b = (int) (Math.random() *(256));
+
+			// color module
+			int r = (int) (Math.random() * (256)); // Choose a random color between 0 and 255 (RGB colors 0-255)
+			int gr = (int) (Math.random() * (256));
+			int b = (int) (Math.random() * (256));
 			Color colour = new Color(r, gr, b); // define colour as red green and blue numbers
 			g.setColor(colour); // set the color of them lines to that random color we made
-			
-		int x = 400 + (int) (Math.random() *(800 - 475) + 1);// location for s4s
-		int y = 10 + (int) (Math.random() *(250 - 10) + 1);
-		g.fillRect(x, y, 50, 50);//size and creation of s4
+
+			int x = 400 + (int) (Math.random() * (800 - 475) + 1);// location for s4s
+			int y = 10 + (int) (Math.random() * (250 - 10) + 1);
+			g.fillRect(x, y, 50, 50);// size and creation of s4
 		}
 
 		// Draw Random Circles
 		for (int k = 1; k <= 100; k++) {
-			
-			int r = (int) (Math.random() *(256));
-			int gr = (int) (Math.random() *(256));
-			int b = (int) (Math.random() *(256));
-			
-			Color colour = new Color(r, gr, b); 
-			g.setColor(colour); 
-			
-			
-			/*
-			  more to ignore:
-			 
-			 Lab06vst.color(colour);
-			g.setColor(colour); // set the color of them lines to that random color we made*/
 
-			
-	
-			int d = (int) (Math.random() *(200) + 1);	// define d as the random diameter of the circle
-			int x = 400 - d; //formula to prevent the circle being too large and going out of the box
+			int r = (int) (Math.random() * (256));
+			int gr = (int) (Math.random() * (256));
+			int b = (int) (Math.random() * (256));
+
+			Color colour = new Color(r, gr, b);
+			g.setColor(colour);
+
+			/*
+			 * more to ignore:
+			 * 
+			 * Lab06vst.color(colour); g.setColor(colour); // set the color of them lines to
+			 * that random color we made
+			 */
+
+			int d = (int) (Math.random() * (200) + 1); // define d as the random diameter of the circle
+			int x = 400 - d; // formula to prevent the circle being too large and going out of the box
 			int y = 600 - d;
-			
-			int xPos = 10 + (int) (Math.random() * (x - 10) + 1); //random pos within our x that was given via formula
-			int yPos = 300 + (int) (Math.random() * (y - 310) + 1); //same for y
+
+			int xPos = 10 + (int) (Math.random() * (x - 10) + 1); // random pos within our x that was given via formula
+			int yPos = 300 + (int) (Math.random() * (y - 310) + 1); // same for y
 
 			g.drawOval(xPos, yPos, d, d); // draw the circle
-			
+
 		}
 
 		// Draw 3-D Box
+
+		// s3 polygon
+		g.setColor(Color.BLUE); // blue color
+		Polygon s3 = new Polygon();
+		s3.addPoint(650, 350);
+		s3.addPoint(600, 400);
+		s3.addPoint(650, 450);
+		s3.addPoint(700, 400);
+		g.fillPolygon(s3);
+		// square polygon
 		
-		//s1azoid polygon
-		g.setColor(Color.YELLOW); //yellow color
-		Polygon s1 = new Polygon();
-		s1.addPoint(550, 350);
-		s1.addPoint(650, 350);
-		s1.addPoint(650, 400);
-		s1.addPoint(600, 400);
-		s1.addPoint(550, 350);
-		g.fillPolygon(s1);
-		
-		//s2 polygon
-		g.setColor(Color.GREEN); //green color
+		g.setColor(Color.YELLOW); // yellow color
+		g.fillRect(550, 350, 100, 100);
+		// s2 polygon
+		g.setColor(Color.GREEN); // green color
 		Polygon s2 = new Polygon();
 		s2.addPoint(550, 350);
 		s2.addPoint(600, 400);
 		s2.addPoint(600, 500);
-		s2.addPoint(550, 450);
+		s2.addPoint(550, 450); 
 		g.fillPolygon(s2);
 
-		//s4 polygon
-		g.setColor(Color.RED);//red color
+		// s4 polygon
+		g.setColor(Color.RED); // red color square done with polygons cause why not
 		Polygon s4 = new Polygon();
 		s4.addPoint(600, 400);
 		s4.addPoint(600, 500);
@@ -126,13 +128,6 @@ public class Lab06vst extends Applet {
 		s4.addPoint(700, 400);
 		g.fillPolygon(s4);
 		
-		//s3 polygon
-		g.setColor(Color.BLUE);		//blue color
-		Polygon s3 = new Polygon();
-		s3.addPoint(650, 350);
-		s3.addPoint(650, 400);
-		s3.addPoint(700, 400);
-		g.fillPolygon(s3);	
 
 	}
 
