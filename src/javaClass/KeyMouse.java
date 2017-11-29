1527babe2072be57d4f005f1b9a1bf45ed2fa27f
Lego Dude <kushdalal007@gmail.com>
2017-11-29 10:55:20 -0500

    Workign on snake

--- src/javaClass/KeyMouse.java
+++ src/javaClass/KeyMouse.java
@@ -8,74 +8,9 @@ import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
 public class KeyMouse extends JFrame {
-<<<<<<< HEAD
-//Variables
-
-    int y, x;
-//Double Buffer
-    private Image dbImage;
-    private Graphics dbg;
-
-//Window Basics
-    public KeyMouse() {
-        addKeyListener(new AL());
-        setTitle("Add window title");
-        setSize(800, 700);
-        setResizable(false);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        x = 150;
-        y = 150;
-
-    }
 
 
-    public static void main(String[] args) {
-        new KeyMouse();
-    }
-    //Controls
-    public class AL extends KeyAdapter {
 
-        @Override
-        public void keyPressed(KeyEvent event) {
-            int keyCode = event.getKeyCode();
-            if (keyCode == KeyEvent.VK_LEFT)
-            {
-                x-=5;
-            }
-            if (keyCode == KeyEvent.VK_RIGHT)
-            {
-                x+=5;
-            }
-            if (keyCode == KeyEvent.VK_UP)
-            {
-                y-=5;
-            }
-            if (keyCode == KeyEvent.VK_DOWN)
-            {
-                y+=5;
-            }
-        }
-
-        @Override
-        public void keyReleased(KeyEvent event) {
-        }
-    }
-
-
-    @Override
-    public void paint(Graphics g) {
-        dbImage = createImage(getWidth(), getHeight());
-        dbg = dbImage.getGraphics();
-        paintComponent(dbg);
-        g.drawImage(dbImage, 0, 0, this);
-    }
-
-    public void paintComponent(Graphics g) {
-        g.fillOval(x, y, 15, 15);
-        repaint();
-    }
-=======
 	public static void delay(int x) {
 		try {
 			Thread.sleep(x);
@@ -143,40 +78,57 @@ public class KeyMouse extends JFrame {
 
 	public void paintComponenet(Graphics g) {
 		if (b == 1) {
-
-			System.out.println(x);
+			
 			x -= 2;
 			repaint();
 			g.fillOval(x, y, 15, 15);
 			if (x < 10 || y < 10 || x > 800 || y > 700){
+				repaint();
+				g.drawString("Game Over", 400, 350);
+				repaint();
 				KeyMouse.delay(1000000000);
-
+				
 			}
 
 		} else if (b == 0) {
+			System.out.println("y" + y);
+			System.out.println("x" + x);
 			x += 2;
 			repaint();
 			g.fillOval(x, y, 15, 15);
 			if (x < 10 || y < 10 || x > 800 || y > 700){
+				repaint();
+				g.drawString("Game Over", 400, 350);
+				repaint();
 				KeyMouse.delay(1000000000);
+				
 			}
 		}else if (b == 2){
-			System.out.println(x);
+			System.out.println("y" + y);
+			System.out.println("x" + x);
 			y -= 2;
 			repaint();
 			g.fillOval(x, y, 15, 15);
 			if (x < 10 || y < 10 || x > 800 || y > 700){
+				repaint();
+				g.drawString("Game Over", 400, 350);
+				repaint();
 				KeyMouse.delay(1000000000);
+				
 
 			}
 		}else if(b == 3){
-			System.out.println(x);
+			System.out.println("y" + y);
+			System.out.println("x" + x);
 			y += 2;
 			repaint();
 			g.fillOval(x, y, 15, 15);
 			if (x < 10 || y < 10 || x > 800 || y > 700){
+				repaint();
+				g.drawString("Game Over", 400, 350);
+				repaint();
 				KeyMouse.delay(1000000000);
-
+				
 			}
 		} 
 			
@@ -185,5 +137,5 @@ public class KeyMouse extends JFrame {
 
 	}
 
->>>>>>> 72e294f1e40447f84701997d79dc446bbc96328c
+
 }
