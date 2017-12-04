9d9795b92b35577c0f0872fc53d8b0432d314f7d
Lego Dude <kushdalal007@gmail.com>
2017-12-04 14:04:44 -0500

    Adding story line

--- src/javaClass/KeyMouse.java
+++ src/javaClass/KeyMouse.java
@@ -24,35 +24,41 @@ public class KeyMouse extends JFrame {
 	}
 	// Variables
 
-	int y, x, b, score;
+	int y, x, b, score, story;
 	double s;
 	Random rand = new Random();
 	int rx = rand.nextInt(920) + 30;
 	Random rand1 = new Random();
 	int ry = rand1.nextInt(620) + 30;
 
+	Random rand2 = new Random();
+	int by = rand2.nextInt(620) + 30;
+	Random rand3 = new Random();
+	int bx = rand3.nextInt(920) + 30;
+	
 	// Double Buffer
 	private Image dbImage;
 	private Graphics dbg;
 
-	// Window Basics
+	// Window stuff
 	public KeyMouse() {
 		addKeyListener(new Key());
-		setTitle("Snake");
+		setTitle("Border Patrol");
 		setSize(1000, 700);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		x = 150;
 		y = 150;
-		b = 0;
+		b = 10;
 		s = 1;
+		story = 0;
 
 	}
 
 	public static void main(String[] args) {
 		new KeyMouse();
-	
+		//main to run it
 	}
 
 	// Controls
@@ -61,23 +67,31 @@ public class KeyMouse extends JFrame {
 		public void keyPressed(KeyEvent event) {
 
 			int keyCode = event.getKeyCode();
+			
 			if (keyCode == KeyEvent.VK_LEFT) {
 				b = 1;
-
+				story = 1;
 			}
 			if (keyCode == KeyEvent.VK_RIGHT) {
 				b = 0;
+				story = 1;
+
 			}
 			if (keyCode == KeyEvent.VK_UP) {
 				b = 2;
+				story = 1;
+
 			}
 			if (keyCode == KeyEvent.VK_DOWN) {
 				b = 3;
+				story = 1;
+
 			}
 
 		}
 
 	}
+	
 	@Override
 	public void paint(Graphics g) {
 		dbImage = createImage(getWidth(), getHeight());
@@ -87,6 +101,7 @@ public class KeyMouse extends JFrame {
 		g.setColor(Color.YELLOW);
 		g.setFont(new Font("Open Sans", Font.BOLD, 22));
 		g.drawString("Score: " + score, 50, 50);
+		//create screen
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -94,30 +109,45 @@ public class KeyMouse extends JFrame {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, 1000, 800);
 		g.setColor(Color.GREEN);
-	
+		//draw background 
+		g.setColor(Color.GRAY);
+		g.fillRect(375, 500, 300, 150);
+		g.setFont(new Font("Courier New", Font.BOLD, 12));
+		g.setColor(Color.BLUE);
+		g.drawString("You are a A border patrol agent,(Blue Dot)", 375, 530);
 		
-
+		
+		
+//check for movement and scoring
+		if(b == 10) {
+			repaint();
+			g.setColor(Color.BLUE); //player
+			g.fillOval(x, y, 20, 20);
+			g.setColor(Color.ORANGE); //objective
+			g.fillOval(rx, ry, 15, 15);
+		}
 		if (b == 1) {
 			
 			x -= s;
 			repaint();
-			g.setColor(Color.GREEN);
-			g.fillOval(x, y, 15, 15);
-			g.setColor(Color.ORANGE);
+			g.setColor(Color.BLUE); //player
+			g.fillOval(x, y, 20, 20);
+			g.setColor(Color.ORANGE); //objective
 			g.fillOval(rx, ry, 15, 15);
-			if((x >= rx - 15 && x <= rx+15) && (y >= ry - 15 && y <= ry + 15)) {
 			
-				score += 1;
-				s+=0.5;
+			if((x >= rx - 15 && x <= rx+15) && (y >= ry - 15 && y <= ry + 15)) { //check if collected
+			
+				score += 1; //score modifier
+				s+=0.5; //speed modifier
 				System.out.println("Score: " + score);
-				System.out.println("Speed: " + s);
-				g.setColor(Color.BLACK);
+				System.out.println("Speed: " + s); 
+				g.setColor(Color.BLACK); // object creators
 				g.fillOval(rx, ry, 15, 15);
 				rx = rand.nextInt(920) + 30;
 				ry = rand1.nextInt(620) + 30;
-				g.setColor(Color.GREEN);
+				g.setColor(Color.BLUE);
 			}
-			if (x < 0 || y < 25 || x > 980 || y > 680){
+			if (x < 0 || y < 25 || x > 980 || y > 680){ //border patrol. 
 				repaint();
 				g.drawString("Game Over", 400, 350);
 				repaint();
@@ -132,8 +162,8 @@ public class KeyMouse extends JFrame {
 			//System.out.println("x" + x);
 			x += s;
 			repaint();
-			g.setColor(Color.GREEN);
-			g.fillOval(x, y, 15, 15);
+			g.setColor(Color.BLUE);
+			g.fillOval(x, y, 20, 20);
 			g.setColor(Color.ORANGE);
 			g.fillOval(rx, ry, 15, 15);
 			if((x >= rx - 15 && x <= rx+15) && (y >= ry - 15 && y <= ry + 15)) {
@@ -146,7 +176,7 @@ public class KeyMouse extends JFrame {
 				g.fillOval(rx, ry, 15, 15);
 				rx = rand.nextInt(920) + 30;
 				ry = rand1.nextInt(620) + 30;
-				g.setColor(Color.GREEN);
+				g.setColor(Color.BLUE);
 			}
 			if (x < 0 || y < 25 || x > 980 || y > 680) {
 				repaint();
@@ -163,8 +193,8 @@ public class KeyMouse extends JFrame {
 			//System.out.println("x" + x);
 			y -= s;
 			repaint();
-			g.setColor(Color.GREEN);
-			g.fillOval(x, y, 15, 15);
+			g.setColor(Color.BLUE);
+			g.fillOval(x, y, 20, 20);
 			g.setColor(Color.ORANGE);
 			g.fillOval(rx, ry, 15, 15);
 			if((x >= rx - 15 && x <= rx+15) && (y >= ry - 15 && y <= ry + 15)) {
@@ -177,7 +207,7 @@ public class KeyMouse extends JFrame {
 				g.fillOval(rx, ry, 15, 15);
 				rx = rand.nextInt(920) + 30;
 				ry = rand1.nextInt(620) + 30;
-				g.setColor(Color.GREEN);
+				g.setColor(Color.BLUE);
 			}
 			if (x < 0 || y < 25 || x > 980 || y > 680 ){
 				repaint();
@@ -195,8 +225,8 @@ public class KeyMouse extends JFrame {
 			//System.out.println("x" + x);
 			y += s;
 			repaint();
-			g.setColor(Color.GREEN);
-			g.fillOval(x, y, 15, 15);
+			g.setColor(Color.BLUE);
+			g.fillOval(x, y, 20, 20);
 			g.setColor(Color.ORANGE);
 			g.fillOval(rx, ry, 15, 15);
 			
@@ -210,7 +240,7 @@ public class KeyMouse extends JFrame {
 				g.fillOval(rx, ry, 15, 15);
 				rx = rand.nextInt(920) + 30;
 				ry = rand1.nextInt(620) + 30;
-				g.setColor(Color.GREEN);
+				g.setColor(Color.BLUE);
 
 			}
 			
