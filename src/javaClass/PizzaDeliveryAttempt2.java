494b7d428054e62d005db2091e78dfc54c98f133
Ben Sabo <sabob11057@stxavier.org>
2017-12-06 08:56:02 -0500

    Did stuff

--- src/javaClass/PizzaDeliveryAttempt2.java
+++ src/javaClass/PizzaDeliveryAttempt2.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class PizzaDeliveryAttempt2 {
 
+	
+
 	static boolean pizza = false;
 	static boolean salad = false;
 	static boolean wings = false;
@@ -19,6 +21,11 @@ public class PizzaDeliveryAttempt2 {
 	static boolean mildSauce = false;
 	static boolean mediumSauce = false;
 	static boolean hotSauce = false;
+	
+	static boolean boneYes = false;
+	static boolean boneNo = false;
+	
+	
 
 	public static void main(String[] args) {
 		
@@ -36,13 +43,14 @@ public class PizzaDeliveryAttempt2 {
 			
 			if (typeFood.equalsIgnoreCase("pizza")) {
 				pizza = true;
-				pizzaSizeScan();
+				PizzaScans.pizzaSizeScan();
 			} 
 			else if (typeFood.equalsIgnoreCase("wings")) {
 				wings = true;
-				wingsSauceScan();
+				WingsScans.wingsSauceScan();
 			} else if (typeFood.equalsIgnoreCase("salad")) {
 				salad = true;
+				SaladScans.dressingScan();
 			} else {
 				System.out.println("That is not one of the options");
 				firstScan();
@@ -52,76 +60,4 @@ public class PizzaDeliveryAttempt2 {
 		while (pizza == false && salad == false && wings == false);
 		
 		}
-	
-	public static void pizzaSizeScan() {
-		
-		do  {
-			
-			System.out.println("We have 3 sizes: 9\", 12\", and 16\"");
-			Scanner sizeInput = new Scanner(System.in);
-			String pizzaSize = sizeInput.nextLine();
-			
-			if (pizzaSize.equalsIgnoreCase("9")) {
-				nineInch = true;
-				pizzaToppingScan();
-			} else if (pizzaSize.equalsIgnoreCase("12")) {
-				twelveInch = true;
-				pizzaToppingScan();
-			} else if (pizzaSize.equalsIgnoreCase("16")) {
-				sixteenInch = true;
-				pizzaToppingScan();
-			} else {
-				System.out.println("Please choose a valid size");
-				pizzaSizeScan();
-			}
-			
-		}
-		while (nineInch == false && twelveInch == false && sixteenInch == false);
-		
-	}
-
-	public static void pizzaToppingScan() {
-		
-		do {	
-		System.out.println("We have three topping choices: pepperoni, sausage, and bacon");
-		Scanner toppingInput = new Scanner(System.in);
-		String topping = toppingInput.nextLine();
-			if (topping.equalsIgnoreCase("pepperoni")) {
-				pepperoni = true;
-			} else if (topping.equalsIgnoreCase("sausage")) {
-				sausage = true;
-			} else if (topping.equalsIgnoreCase("bacon")) {
-				bacon = true;
-			} else {
-				System.out.println("That is not one of our toppings");
-				topping = toppingInput.nextLine();
-			}
-		}
-		while (pepperoni == false && sausage == false && bacon == false);
-	
-	}
-	
-	public static void wingsSauceScan() {
-		
-		do {
-			System.out.println("What sauce would you like? We have a mild, medium, and hot sauce");
-			Scanner sauceInput = new Scanner(System.in);
-			String sauce = sauceInput.nextLine();
-				if(sauce.equalsIgnoreCase("mild")) {
-					mildSauce = true;
-				}
-				else if(sauce.equalsIgnoreCase("medium")) {
-					mediumSauce = true;
-				}
-				else if(sauce.equalsIgnoreCase("hot")) {
-					hotSauce = true;
-				}
-				else {
-					System.out.println("That is not a sauce type");
-					wingsSauceScan();
-				}
-		}
-		while(mildSauce == false && mediumSauce == false && hotSauce == false);
-	}
-
 }
---
+++ src/javaClass/PizzaScans.java
@@ -0,0 +1,56 @@
+package javaClass;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class PizzaScans extends PizzaDeliveryAttempt2 {
+	
+	public static void pizzaSizeScan() {
+		
+		do  {
+			
+			System.out.println("We have 3 sizes: 9\", 12\", and 16\"");
+			Scanner sizeInput = new Scanner(System.in);
+			String pizzaSize = sizeInput.nextLine();
+			
+			if (pizzaSize.equalsIgnoreCase("9")) {
+				nineInch = true;
+				pizzaToppingScan();
+			} else if (pizzaSize.equalsIgnoreCase("12")) {
+				twelveInch = true;
+				pizzaToppingScan();
+			} else if (pizzaSize.equalsIgnoreCase("16")) {
+				sixteenInch = true;
+				pizzaToppingScan();
+			} else {
+				System.out.println("Please choose a valid size");
+				pizzaSizeScan();
+			}
+			
+		}
+		while (nineInch == false && twelveInch == false && sixteenInch == false);
+		
+	}
+
+	public static void pizzaToppingScan() {
+		
+		do {	
+		System.out.println("We have three topping choices: pepperoni, sausage, and bacon");
+		Scanner toppingInput = new Scanner(System.in);
+		String topping = toppingInput.nextLine();
+			if (topping.equalsIgnoreCase("pepperoni")) {
+				pepperoni = true;
+			} else if (topping.equalsIgnoreCase("sausage")) {
+				sausage = true;
+			} else if (topping.equalsIgnoreCase("bacon")) {
+				bacon = true;
+			} else {
+				System.out.println("That is not one of our toppings");
+				topping = toppingInput.nextLine();
+			}
+		}
+		while (pepperoni == false && sausage == false && bacon == false);
+	
+	}
+
+}
